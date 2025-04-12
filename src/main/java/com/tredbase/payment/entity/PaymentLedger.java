92474c9f5cdd9a607f17ddc4cf3d4a9a646ca746
DanielOpara <oparadaniv@gmail.com>
2025-04-12 17:49:37 +0100

    adding payment ledger

--- src/main/java/com/tredbase/payment/entity/PaymentLedger.java
+++ src/main/java/com/tredbase/payment/entity/PaymentLedger.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,9 +11,6 @@ import java.util.UUID;
 
 @Entity
 public class PaymentLedger {
-
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
